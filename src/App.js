821b127399fa8c050ea6087233911d9bd627634f
Damian Lis <foxprogramming93@gmail.com>
2021-06-23 19:21:49 +0200

    Set gql client in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Home from 'pages/Home';
 import Search from 'pages/Search';
 
 function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: 'https://countries.trevorblades.com',
+  });
   return (
-    <div className='App'>
-      <Router>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/search' exact component={Search} />
-        </Switch>
-      </Router>
-    </div>
+    <ApolloProvider client={client}>
+      <div className='App'>
+        <Router>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/search' exact component={Search} />
+          </Switch>
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 }
 
