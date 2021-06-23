c1ce2a33f50b22763ff5492e9813a76d8e32b7e4
Damian Lis <foxprogramming93@gmail.com>
2021-06-23 18:17:59 +0200

    Created basic structure and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from 'pages/Home';
+import Search from 'pages/Search';
 
 function App() {
-  return <div className='App'> Hello World!</div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/search' exact component={Search} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
