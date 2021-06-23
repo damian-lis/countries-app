be4f4b82ae0fb43f6fa68716f93eea858c1f34e1
Damian Lis <foxprogramming93@gmail.com>
2021-06-23 19:21:00 +0200

    Created  Search page with fetch specific country data using gql

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLazyQuery, gql } from '@apollo/client';
+import { Link } from 'react-router-dom';
 
-const Search = () => {
-  return <div>Search</div>;
-};
+const QUERY_SEARCH_COUNTRY = gql`
+  query Country($code: ID!) {
+    country(code: $code) {
+      name
+      capital
+      emoji
+      code
+      currency
+    }
+  }
+`;
+
+function Search() {
+  const [countrySearch, setCountrySearch] = useState('');
+  const [searchCountry, { data }] = useLazyQuery(QUERY_SEARCH_COUNTRY);
+
+  const displayCountry = data && data[Object.keys(data)];
+
+  return (
+    <div className='search'>
+      <div className='inputs'>
+        <Link to='/'> List of Countries</Link>
+        <input
+          type='text'
+          placeholder='Enter Country Code (ex. BR)...'
+          onChange={(event) => {
+            setCountrySearch(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            if (!countrySearch) return alert('You must write country!');
+            searchCountry({
+              variables: { code: countrySearch.toUpperCase() },
+            });
+          }}
+        >
+          {' '}
+          Search Country
+        </button>
+      </div>
+
+      <div className='searchCountry'>
+        {displayCountry && (
+          <div className='countryDisplay'>
+            <h1>
+              {data.country.name}
+              {data.country.emoji}
+            </h1>
+            <h1>Capital: {data.country.capital} </h1>
+            <h1> Currency: {data.country.currency}</h1>
+            <h1> Country Code: {data.country.code}</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
 
 export default Search;
